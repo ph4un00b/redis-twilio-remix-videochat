3fbcd214ebf2a60c5fd07ff2a94d0aa89fe5302a
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-12 14:54:23 -0500

    chore(Root): add chat users on db

--- app/routes/chat/$chatId.tsx
+++ app/routes/chat/$chatId.tsx
@@ -1,9 +1,50 @@
 import { ActionFunction, LoaderFunction } from '@remix-run/node'
 import { useParams } from '@remix-run/react'
+import { Redis } from '@upstash/redis'
+import * as yup from 'yup'
+
 export const loader: LoaderFunction = async ({
   params
 }) => {
+  const schema = yup.object().shape({
+    UPSTASH_REDIS_REST_URL: yup
+      .string()
+      .url()
+      .required(),
+    UPSTASH_REDIS_REST_TOKEN: yup
+      .string()
+      .required()
+  })
+
+  const env = schema.validateSync({
+    UPSTASH_REDIS_REST_URL: process
+      .env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process
+      .env.UPSTASH_REDIS_REST_TOKEN
+  })
+
+  const redis = new Redis({
+    url: env.UPSTASH_REDIS_REST_URL,
+    token: env.UPSTASH_REDIS_REST_TOKEN
+  })
+
   console.log(params.chatId)
+  let chatUsers: string[] | null =
+    await redis.get('room:midu:users')
+
+  // init chat
+  if (chatUsers === null) {
+    chatUsers = []
+  }
+
+  // add user once
+  if (chatUsers.findIndex((v) => v === 'phau') < 0) {
+    chatUsers.push('phau')
+  }
+
+  await redis.set('room:midu:users', JSON.stringify(chatUsers))
+
+  console.log('chat-users', chatUsers)
   return {}
 }
 
