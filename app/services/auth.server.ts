d658b63b8b0a4b3a1a141d8edb50d4e5b02ac9ca
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-19 14:41:34 -0500

    chore(Root): export base url as env var

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -5,21 +5,25 @@ import { storage } from './sessions.server'
 
 const schema = yup.object().shape({
   GITHUB_CLIENT_ID: yup.string().required(),
-  GITHUB_CLIENT_SECRET: yup.string().required()
+  GITHUB_CLIENT_SECRET: yup.string().required(),
+  // BASE_URL: yup.string().url().required()
+  BASE_URL: yup.string().required()
 })
 
 const env = schema.validateSync({
   GITHUB_CLIENT_ID: process
     .env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process
-    .env.GITHUB_CLIENT_SECRET
+    .env.GITHUB_CLIENT_SECRET,
+  BASE_URL: process
+    .env.BASE_URL
 })
 
 const gitHubStrategy = new GitHubStrategy(
   {
     clientID: env.GITHUB_CLIENT_ID,
     clientSecret: env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
+    callbackURL: `${env.BASE_URL}/auth/github/callback`
   },
   async ({ accessToken, extraParams, profile }) => {
     // Get the user data from your DB or API using the tokens and profile
